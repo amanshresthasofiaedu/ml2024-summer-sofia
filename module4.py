796ffd7896d19ead3fa32ac8e65ba98ad8fad314
Aman Shrestha <amanshrestha@google.com>
2024-10-25 18:48:58 +0000

    changed the index response to be between 1 and N

--- module4.py
+++ module4.py
@@ -1,18 +1,18 @@
 def createList():
-    size = int(input("Enter the number of items to be in the list"))
+    size = int(input("Enter the number of items to be in the list "))
     mainList = [0] * size
     for i in range (0, size):
-        mainList[i] = int(input("Enter an item to store in the list"))
+        mainList[i] = int(input("Enter an item to store in the list "))
     
     return mainList
 
 def findElement(list, value):
     for i in range(0,len(list)):
         if(list[i] == value):
-            return i
+            return i  + 1 # Response needs to be 1 to N inclusive
     return -1
 
 mainList = createList()
-toFind = int(input("Now enter an item you'd like to search"))
+toFind = int(input("Now enter an item you'd like to search "))
 print(f"You can find the element at {findElement(mainList, toFind)}")
 
