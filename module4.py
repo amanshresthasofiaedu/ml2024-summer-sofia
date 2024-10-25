42692c4ba71347d54503709bab6e8f5bc82fb33f
Aman Shrestha <amanshrestha@google.com>
2024-10-25 18:44:43 +0000

    Created the python file with the assignment

---
+++ module4.py
@@ -0,0 +1,18 @@
+def createList():
+    size = int(input("Enter the number of items to be in the list"))
+    mainList = [0] * size
+    for i in range (0, size):
+        mainList[i] = int(input("Enter an item to store in the list"))
+    
+    return mainList
+
+def findElement(list, value):
+    for i in range(0,len(list)):
+        if(list[i] == value):
+            return i
+    return -1
+
+mainList = createList()
+toFind = int(input("Now enter an item you'd like to search"))
+print(f"You can find the element at {findElement(mainList, toFind)}")
+
